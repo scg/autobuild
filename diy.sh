e9694d35f7619cd957a3e1645fc7a79cf4db76d0
Wildlife <lanseyujie@users.noreply.github.com>
2020-03-02 16:43:14 +0800

    Create diy.sh

---
+++ diy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo '修改网关地址'
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+
+echo '修改时区'
+sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
+© 2020 GitHub, Inc.
